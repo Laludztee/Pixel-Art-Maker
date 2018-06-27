c979eaf51dd74525814bbebdd852b3772e0bc0ef
Laludztee <oluwatomisin1605@gmail.com>
2018-06-27 18:18:33 +0100

    Using const and let for my identifiers

--- designs.js
+++ designs.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 const height = $('#inputHeight');
 const width = $('#inputWidth');
 // Select color
-let colorPicker = $('#colorPicker');
+const colorPicker = $('#colorPicker');
 // Select canvas
 const canvas = $('#pixelCanvas');
 
@@ -17,17 +17,17 @@ function makeGrid() {
     canvas.append('<tbody></tbody>');
     let canvasBody = canvas.find('tbody');
     // Draw rows
-    for (let x = 1; x <= gridWidth; x++) {
+    for (let x = 0; x < gridWidth; x++) {
     canvasBody.append('<tr></tr>');
     }
     // Draw columns
-    for (y = 1; y <= gridHeight; y++) {
+    for (let y = 0; y < gridHeight; y++) {
     canvas.find('tr').append('<td></td>');
     }
 }
 // The color to be shown when the user selects a table data
 canvas.on('click', "td", function(){
-    var color = colorPicker.val();
+    let color = colorPicker.val();
     $(this).css("background-color", color);
 });
 // The color to be shown when the user de-selects a table data
